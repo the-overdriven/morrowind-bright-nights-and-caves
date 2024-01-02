79c9a07f718bb321df4d412f29e7e1fc19fc1a67
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-01-02 21:05:43 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,3 +6,38 @@ p0:createDecimalSlider{label = "Gamma", max = 5, variable = var{id = "gam", tabl
 p0:createDecimalSlider{label = "Saturation", max = 5, variable = var{id = "sat", table = cf}, callback = function() GAM.saturation = cf.sat end}
 p0:createDecimalSlider{label = "Luminance", max = 5, variable = var{id = "lum", table = cf}, callback = function() GAM.luminance = cf.lum end}
 end		event.register("modConfigReady", registerModConfig)
+
+local function getDayTime()
+    local hour = tes3.worldController.hour.value
+    local wc = tes3.worldController.weatherController
+
+    local sunriseBegin = wc.sunriseHour
+    local sunriseDuration = wc.sunsetDuration
+    local sunriseEnd = sunriseBegin + sunriseDuration
+
+    local sunsetBegin = wc.sunsetHour
+    local sunsetDuration = wc.sunsetDuration
+    local sunsetEnd = sunsetBegin + sunsetDuration
+
+    if (hour >= sunriseBegin and hour < sunriseEnd) then
+        return "sunrise"
+    elseif (hour >= sunriseEnd and hour < sunsetBegin) then
+        return "day"
+    elseif (hour >= sunsetBegin and hour < sunsetEnd) then
+        return "sunset"
+    else
+        return "night"
+    end
+end
+
+local function cellChangedCallback(e)
+	if (e.cell.isOrBehavesAsExterior and getDayTime() == "day") then
+		-- decrease luminance in exteriors when it is day
+		GAM.luminance = 1.10
+	else
+		-- increase luminance in interiors, or when it is night
+		GAM.luminance = 1.50
+	end
+end
+
+event.register(tes3.event.cellChanged, cellChangedCallback)
